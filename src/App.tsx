680fd530b9850970eaa74d6121d46e54ebafcea3
Bruno Grassano <bgrassano@fi.uba.ar>
2022-10-17 21:19:54 -0300

    Add services to view

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Register from './Register';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Login from "./views/auth/Login";
 import { Rental } from './views/rentals/Rental';
+import { sampleData } from './sampleData/Rentals';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route path="/" element={<Register />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/rental" element={<Rental />}/>
+          <Route path="/rental" element={<Rental {...sampleData[0]} />}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/views/rentals/Rental.tsx
+++ src/views/rentals/Rental.tsx
@@ -1,37 +1,90 @@
-import React from "react"
-import "./rental.css"
+import { IconButton,  Tooltip } from "@mui/material";
+import { BsWifi,  BsFillHouseFill, BsBuilding } from "react-icons/bs";
+import {FaDog, FaTshirt, FaSink} from "react-icons/fa";
+import {FiMonitor} from "react-icons/fi";
+import {AiFillCar} from "react-icons/ai";
+import React from "react";
+import { Rental as IRental } from "../../types/Rentals";
+import "./rental.css";
 
-export const Rental = () => {
+
+const getRentalTypeIcon = (rental: IRental): React.ReactNode => {
+    if (rental.propertyType == "APARTMENT" ){
+        return (
+            <Tooltip title="Departamento">
+                <IconButton>
+                    <BsBuilding size={"40px"} color="#3E3E3E" />
+                </IconButton>
+            </Tooltip>
+        )
+    }
+    return (
+        <Tooltip title="Casa">
+            <IconButton>
+                <BsFillHouseFill size={"40px"} color="#3E3E3E" />
+            </IconButton>
+        </Tooltip>
+        );
+}
+
+const mapServicesToText = (service : string) => {
+    if(service=="WIFI"){
+        return <li className="rental-description-text"><BsWifi /> WIFI</li>
+    }
+    if(service=="TV"){
+        return <li className="rental-description-text"><FiMonitor /> TV</li>
+    }
+    if(service=="GARAGE"){
+        return <li className="rental-description-text"><AiFillCar /> Garage</li>
+    }
+    if(service=="KITCHEN"){
+        return <li className="rental-description-text"><FaSink /> Cocina</li>
+    }
+    if(service=="LAUNDRY"){
+        return <li className="rental-description-text"><FaTshirt /> Lavarropas</li>
+    }
+    return <li className="rental-description-text"><FaDog /> Pet Friendly</li>
+}
+
+const renderServices = (rental: IRental): React.ReactNode => {
+    let services : string[]= rental.services ? [...rental.services] : []
+    rental.garageAvailable && services.push("GARAGE")
+    rental.petsAvailable && services.push("PET FRIENDLY") 
+
+    return services.map(mapServicesToText);
+}
+
+export const Rental = (data : IRental) => {
+    
     return <div className="rental">
-        <div className="rental-header">
+        <div className="row rental-header">
 
-
-            <div >
+            <div className="col-7">
                 <h2 className="rental-address">Argentina, CABA</h2>
                 <h2 className="rental-address">Av. de Mayo 254 - 3 piso</h2>
             </div>
 
-            <div>
-                <h2 className="rental-price">$200 USD noche</h2>
-                <button className="rental-reservation-button">Reservar </button>
+            <div className="col-5">
+                <h2 className="rental-price">$200 USD noche {getRentalTypeIcon(data)}</h2>
+                <button className="rental-reservation-button">Reservar</button>
             </div>
 
 
         </div>
 
-        <div className="rental-data">
-            <div>
+        <div className="row rental-body">
+            <div className="col-5">
             <img
-                    src={'https://media.gettyimages.com/photos/modern-apartment-building-facade-picture-id171354810'}
+                    src={'https://preview.redd.it/1b6g811jhyi51.jpg?auto=webp&s=c3ae56a6f878ea0673076d6b4044ffc5b863baad'}
                     alt="Photo of rental"
-                    style={{ width: 500, borderRadius: 50 }}
+                    style={{ width: 600, borderRadius: 50 }}
                 />
             </div>
 
-            <div>
+            <div className="col-7 rental-data">
                 <div>
                     <h3 className="rental-description-title">Descripcion</h3>
-                    <p className="rental-description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet consectetur adipiscing elit ut aliquam purus sit amet. Pellentesque diam volutpat commodo sed egestas egestas. Et netus et malesuada fames ac turpis egestas sed. Odio morbi quis commodo odio aenean sed adipiscing.</p>
+                    <p className="rental-description-text">{data.description}</p>
                     <ul>
                         <li className="rental-description-text">4 habitaciones</li>
                         <li className="rental-description-text">2 baños</li>
@@ -41,7 +94,7 @@ export const Rental = () => {
                 </div>
                 <div>
                     <h3 className="rental-description-title">Servicios</h3>
-
+                    {renderServices(data)}
                 </div>
             </div>
         </div>
