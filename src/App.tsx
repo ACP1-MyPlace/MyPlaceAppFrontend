3ff2f7a71268ee21ae99edf03d97690324898f12
LeviMatias <matilevi35@gmail.com>
2022-10-18 23:04:34 -0300

    preliminary create new place screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Register from './Register';
+import Place from './Place';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Login from "./views/auth/Login";
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Register />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/newplace" element={<Place />} />
         </Routes>
       </BrowserRouter>
     </div>
