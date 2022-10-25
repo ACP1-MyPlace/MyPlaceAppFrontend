b11b22317788166f90c8f0c88c750be2372ecc24
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-25 15:02:58 -0300

    Add endpoint for booking

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import AuthPage from "./views/auth/AuthPage";
 import { Rental } from './views/rentals/Rental';
 import { sampleData } from './sampleData/Rentals';
+import { Booking } from './views/booking/Booking';
+import { sampleData as bookingSD } from './sampleData/Booking';
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
 
           <Route path="/auth" element={<AuthPage />} />
           <Route path="/rental" element={<Rental {...sampleData[0]} />}/>
+          <Route path="/booking" element={<Booking {...bookingSD}/>}/>
 
         </Routes>
       </BrowserRouter>
