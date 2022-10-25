cbd269b81fb847fd5452df900cdecdbb83bfb45a
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-25 15:02:49 -0300

    Add view and sample data for Booking

---
+++ src/views/booking/Booking.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Booking as IBooking } from "../../types/Booking";
+import "./booking.css";
+
+export const Booking = (data : IBooking) => {
+    return (<div>"Hola mundo!"</div>)
+}
