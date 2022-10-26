6001fa9496559281235637c5dc7af26a577fff0b
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-26 01:21:22 -0300

    Add boking card

--- src/views/booking/Booking.tsx
+++ src/views/booking/Booking.tsx
@@ -1,7 +1,113 @@
 import React from "react";
+import dayjs, { Dayjs } from 'dayjs';
 import { Booking as IBooking } from "../../types/Booking";
+import Checkbox from '@mui/material/Checkbox';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import { Card, CardActions, CardContent, Stack, Button, Typography, TextField, Grid } from "@mui/material";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 import "./booking.css";
 
 export const Booking = (data : IBooking) => {
-    return (<div>"Hola mundo!"</div>)
+
+    const [value, setValue] = React.useState<Dayjs | null>(
+        dayjs('2014-08-18T21:11:54'),
+      );
+    
+      const handleChange = (newValue: Dayjs | null) => {
+        setValue(newValue);
+      };
+
+      
+    return (
+        
+        <Card sx={{ minWidth: '493px' }}>
+                <CardContent>
+                    <Typography variant="h1" color="primary" mb={2}>
+                        MY PLACE
+                    </Typography>
+                    <Grid container spacing={2}>
+                        <Grid item xs={2}/>
+                        <Grid item xs={3}>
+                            <Typography variant="h3" color="primary" mb={2}>
+                                Tus datos
+                            </Typography>
+                            <Stack spacing={2}>
+                                <TextField variant="outlined"
+                                            label="Nombre"
+                                            size="small" />
+                                <TextField variant="outlined"
+                                            label="Apellido"
+                                            size="small" />
+                                <TextField variant="outlined"
+                                            label="Email"
+                                            size="small"/>
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={2}/>
+                        <Grid item xs={3}>
+                            <Typography variant="h3" color="primary" mb={2}>
+                                Fecha de reserva
+                            </Typography>
+                            <Stack spacing={4}>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DesktopDatePicker
+                                        label="Reservar desde"
+                                        inputFormat="DD/MM/YYYY"
+                                        value={value}
+                                        onChange={handleChange}
+                                        renderInput={(params) => <TextField {...params} />}
+                                    />                 
+                                </LocalizationProvider>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DesktopDatePicker
+                                        label="Reservar hasta"
+                                        inputFormat="DD/MM/YYYY"
+                                        value={value}
+                                        onChange={handleChange}
+                                        renderInput={(params) => <TextField {...params} />}
+                                    />                 
+                                </LocalizationProvider>
+                                <div className='payment'>
+                                    {checkbox('Efectivo', true, null)}
+                                    {checkbox('Tarjeta de Credito/Debito', false, null)}
+                                </div>
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={2}/>
+                    </Grid>
+                </CardContent>
+                <CardActions>
+                    <Button fullWidth color="secondary" variant="contained">
+                        CANCELAR
+                    </Button>
+                    <Button fullWidth color="primary" variant="contained">
+                        CONFIRMAR
+                    </Button>
+                </CardActions>
+            </Card>
+    )
+}
+
+function checkbox(name: string, checked: boolean, onChange: any) {
+    return (
+        <FormControlLabel
+            label={name}
+            control={
+                <Checkbox
+                    icon={<RadioButtonUncheckedIcon />}
+                    checkedIcon={<RadioButtonCheckedIcon />}
+                    checked={checked}
+                    onChange={onChange}
+                    sx={{
+                        color: '#747474',
+                        '&.Mui-checked': {color: '#FF385C'}
+                    }}
+                />
+            }/>
+    )
 }
