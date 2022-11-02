96e18b9bdd8d8b115411eca6646b178c2d70bb65
Maxi Romero <mromerov@fi.uba.ar>
2022-11-01 23:46:03 -0300

    Se agrega listado de reservas con diferente vista para el Host o Huesped

--- src/types/PlacesTypes.ts
+++ src/types/PlacesTypes.ts
@@ -1,4 +1,8 @@
-﻿export enum PlaceFields {
+﻿import {User} from "./User";
+
+export enum PlaceFields {
+    Id = 'id',
+    User = 'user',
     UserId = 'userId',
     PropertyType = 'propertyType',
     Country = 'country',
@@ -18,6 +22,25 @@
 
 export interface NewPlaceForm {
     [PlaceFields.UserId]: number,
+    [PlaceFields.PropertyType]: number,
+    [PlaceFields.Country]: string,
+    [PlaceFields.State]: string,
+    [PlaceFields.Street]: string,
+    [PlaceFields.StreetNumber]: number,
+    [PlaceFields.Floor]?: string,
+    [PlaceFields.Apartment]?: string,
+    [PlaceFields.RoomsCount]: number,
+    [PlaceFields.BathroomCount]: number,
+    [PlaceFields.GarageAvailable]: boolean,
+    [PlaceFields.PetsAvailable]: boolean,
+    [PlaceFields.Services]: number[],
+    [PlaceFields.PricePerNight]: PriceType,
+    [PlaceFields.Description]?: string,
+}
+
+export interface Place {
+    [PlaceFields.Id]: number,
+    [PlaceFields.User]: User,
     [PlaceFields.PropertyType]: number,
     [PlaceFields.Country]: string,
     [PlaceFields.State]: string,
---
+++ src/views/reservations/ReservationComponent.tsx
@@ -0,0 +1,91 @@
+﻿import React from 'react';
+import {Button, Card, CardContent, Chip, Grid, Stack, Typography} from "@mui/material";
+import {ReservationFields, ReservationStatus, ReservationView} from "../../types/ReservationTypes";
+import {dateFormatter} from "../../utils/dateFormatter";
+import {PlaceFields} from "../../types/PlacesTypes";
+import {userStorage} from "../../userSession/userStorage";
+
+interface ReservationComponentProps {
+    reservation: ReservationView
+}
+
+function ReservationComponent(props: ReservationComponentProps) {
+    let isHost : boolean = userStorage.isHost();
+
+    const renderDates = () => (
+        <Stack direction='row' spacing={1} alignItems='center'>
+            <Typography fontSize={'1rem'} color={'text.disabled'}>
+                Fecha: del
+            </Typography>
+            <Typography fontSize={'.95rem'} color={'text.disabled'}>
+                {dateFormatter.toShortDate(props.reservation[ReservationFields.StartingDate]) || "-"} al
+            </Typography>
+            <Typography fontSize={'.95rem'} color={'text.disabled'}>
+                {dateFormatter.toShortDate(props.reservation[ReservationFields.FinishingDate]) || "-"}
+            </Typography>
+        </Stack>
+    )
+    
+    const renderButtons = () => (
+        <Stack direction="row" spacing={1} justifyContent='end'>
+            <Button variant="contained" color="error">Rechazar</Button>
+            <Button variant="contained" color="success">Aceptar</Button>
+        </Stack>
+    )
+    
+    
+    
+    const renderState = () => {
+        let colorChip : 'error' | 'info' | 'success' = "info";
+        let labelChip : string = "Pendiente";
+        
+        switch (props.reservation[ReservationFields.Status]) {
+            case ReservationStatus.PENDING:
+                colorChip = 'info';
+                labelChip = 'Pendiente';
+                break;
+            case ReservationStatus.ACCEPTED:
+                colorChip = 'success';
+                labelChip = 'Aceptada';
+                break;
+            case ReservationStatus.NOT_ACCEPTED:
+                colorChip = 'error';
+                labelChip = 'Rechazada';
+                break;
+        }
+        
+        return (
+            <Stack direction="row" spacing={1} justifyContent='end'>
+                <Chip color={colorChip} label={labelChip} />
+            </Stack>
+        );
+    }/**/
+    
+    return (
+        <Card>
+            <CardContent>
+                <Grid container spacing={1}>
+                    <Grid item xs={4}>
+                        <Stack direction="column">
+                            <Typography fontWeight={600} fontSize={'1.15rem'}>
+                                {props.reservation[ReservationFields.Accommodation][PlaceFields.Country]}
+                            </Typography>
+
+                            {renderDates()}
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={4} alignSelf="center" textAlign="center" >
+                        <Typography>
+                            {`Precio por noche: ${props.reservation[ReservationFields.Accommodation][PlaceFields.PricePerNight] || 0}`}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4} alignSelf="center">
+                        { !isHost ? renderButtons() : renderState() }
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default ReservationComponent;
